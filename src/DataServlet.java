ff666f702a4ec01a8814b28d63119fa1bf680dc6
Dariusz.Żulpo <dd2000@wp.pl>
2019-09-01 15:16:17 +0200

    Serwlety 1 września 2019 r.

---
+++ src/DataServlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class DataServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession hs = request.getSession();
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter pw = response.getWriter();
+
+        Date date = (Date) hs.getAttribute("date");
+        if (date != null){
+            pw.println("Ostatni dostęp do strony: "+ date + "<br>");
+        } //if
+
+        date = new Date();
+        hs.setAttribute("date", date);
+        pw.println("<br />");
+
+        pw.close();
+    }
+}
